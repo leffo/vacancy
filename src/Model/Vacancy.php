cafc9febbc76df23090cd20569e72239a4fe8121
Alexandr Yakovlev <leffalt@gmail.com>
2020-10-07 18:06:22 +0700

    add setters for "add" method VacancyController

--- src/Model/Vacancy.php
+++ src/Model/Vacancy.php
@@ -5,6 +5,7 @@ namespace AYakovlev\Model;
 
 
 use AYakovlev\Core\Request;
+use AYakovlev\Exception\InvalidArgumentException;
 
 class Vacancy extends ActiveRecordEntity
 {
@@ -126,8 +127,136 @@ class Vacancy extends ActiveRecordEntity
         return $this->category;
     }
 
+    /**
+     * @param int|mixed|null $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @param int $price
+     */
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @param string $organization
+     */
+    public function setOrganization(string $organization): void
+    {
+        $this->organization = $organization;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @param string $telephone
+     */
+    public function setTelephone(string $telephone): void
+    {
+        $this->telephone = $telephone;
+    }
+
+    /**
+     * @param string $experience
+     */
+    public function setExperience(string $experience): void
+    {
+        $this->experience = $experience;
+    }
+
+    /**
+     * @param string $technology
+     */
+    public function setTechnology(string $technology): void
+    {
+        $this->technology = $technology;
+    }
+
+    /**
+     * @param string $skills
+     */
+    public function setSkills(string $skills): void
+    {
+        $this->skills = $skills;
+    }
+
+    /**
+     * @param string $conditions
+     */
+    public function setConditions(string $conditions): void
+    {
+        $this->conditions = $conditions;
+    }
+
+    /**
+     * @param string $datecreation
+     */
+    public function setDatecreation(string $datecreation): void
+    {
+        $this->datecreation = $datecreation;
+    }
+
+    /**
+     * @param int $reserv
+     */
+    public function setReserv(int $reserv): void
+    {
+        $this->reserv = $reserv;
+    }
+
+    /**
+     * @param string $category
+     */
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
+    }
+
+
+
     protected static function getTableName(): string
     {
         return 'vacancies';
     }
+
+    public static function createFromArray(array $fields, User $author): Article
+    {
+        if (empty($fields['name'])) {
+            throw new InvalidArgumentException('Не передано название вакансии');
+        }
+
+        if (empty($fields['text'])) {
+            throw new InvalidArgumentException('Не передан текст статьи');
+        }
+
+        $article = new Article();
+
+        $article->setAuthor($author);
+        $article->setName($fields['name']);
+        $article->setText($fields['text']);
+
+        $article->save();
+
+        return $article;
+    }
+
 }
