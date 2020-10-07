e3fee5b7d0693e66aa2e56a8cc149ce92d158f84
leffo <leffalt@gmail.com>
2020-10-07 16:35:05 +0700

    add form to add vacancy and show it in Vacancy/add - fix problem

--- src/View/addVacancy.tpl.php
+++ src/View/addVacancy.tpl.php
@@ -5,7 +5,7 @@
  */
 ?>
 
-<?php include __DIR__ . '/../header.php'; ?>
+<?php include 'header.php'; ?>
     <h1>Создание новой вакансии</h1>
 <?php if(!empty($error)): ?>
     <div style="color: red;"><?= $error ?></div>
@@ -15,30 +15,33 @@
         <input type="text" name="name" id="name" value="<?= $_POST['title'] ?? '' ?>" size="50"><br>
         <br>
         <label for="price">Зарплата:</label><br>
-        <input type="text" name="price" id="price" value="><?= $_POST['price'] ?? '' ?>" size="50" <br>
+        <input type="text" name="price" id="price" value="<?= $_POST['price'] ?? '' ?>" size="50" <br>
         <br>
         <label for="organization">Организация:</label><br>
-        <input type="text" name="organization" id="organization" value="><?= $_POST['organization'] ?? '' ?>" size="50" <br>
+        <input type="text" name="organization" id="organization" value="<?= $_POST['organization'] ?? '' ?>" size="50" <br>
         <br>
         <label for="address">Адрес:</label><br>
-        <input type="text" name="address" id="address" value="><?= $_POST['address'] ?? '' ?>" size="50" <br>
+        <input type="text" name="address" id="address" value="<?= $_POST['address'] ?? '' ?>" size="50" <br>
         <br>
         <label for="telephone">Телефон:</label><br>
-        <input type="text" name="telephone" id="telephone" value="><?= $_POST['telephone'] ?? '' ?>" size="50" <br>
+        <input type="text" name="telephone" id="telephone" value="<?= $_POST['telephone'] ?? '' ?>" size="50" <br>
         <br>
         <label for="experience">Требуемый опыт:</label><br>
-        <input type="text" name="experience" id="experience" value="><?= $_POST['experience'] ?? '' ?>" size="50" <br>
+        <input type="text" name="experience" id="experience" value="<?= $_POST['experience'] ?? '' ?>" size="50" <br>
         <br>
         <label for="technology">Технологии:</label><br>
-        <input type="text" name="technology" id="technology" value="><?= $_POST['technology'] ?? '' ?>" size="50" <br>
+        <input type="text" name="technology" id="technology" value="<?= $_POST['technology'] ?? '' ?>" size="50" <br>
         <br>
         <label for="skills">Требуемые навыки:</label><br>
-        <input type="text" name="skills" id="skills" value="><?= $_POST['skills'] ?? '' ?>" size="50" <br>
+        <input type="text" name="skills" id="skills" value="<?= $_POST['skills'] ?? '' ?>" size="50" <br>
         <br>
         <label for="conditions">Условия:</label><br>
-        <input type="text" name="conditions" id="conditions" value="><?= $_POST['conditions'] ?? '' ?>" size="50" <br>
-        <br>
+        <input type="text" name="conditions" id="conditions" value="<?= $_POST['conditions'] ?? '' ?>" size="50" <br>
+        <br><br><br>
+        <label for="category">Категория:</label><br>
+        <input type="text" name="category" id="category" value="<?= $_POST['category'] ?? '' ?>" size="50" <br>
+        <br><br><br>
 
         <input type="submit" value="Создать вакансию">
     </form>
-<?php include __DIR__ . '/../footer.php'; ?>
+<?php include 'footer.php'; ?>
